3e0b1e656a1c94b47748531a68d04e87de0efb3b
SakamotoHiroya <ayorih16@icloud.com>
2023-04-15 01:17:17 +0900

    add dummy block data

---
+++ src/app/dummy_api/BlockData.ts
@@ -0,0 +1,75 @@
+import EthBlockData from "@/types/EthBlockData";
+import arbitrum_logo from "public/img/chain_logo/arbitrum.svg"
+import optimism_logo from "public/img/chain_logo/optimism.svg"
+
+const blockData: EthBlockData[] = [
+    {
+        blockNumber: 12345,
+        l2Chains: [
+            {
+                chainName: "arbitrum",
+                chainLogo: arbitrum_logo,
+                blocks: [
+                    {
+                        blockNumber: 100000,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 100001,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 100002,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 100003,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 100004,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    }
+                ]
+            },
+            {
+                chainName: "optimism",
+                chainLogo: optimism_logo,
+                blocks: [
+                    {
+                        blockNumber: 200000,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 200001,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 200002,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 200003,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    },
+                    {
+                        blockNumber: 200004,
+                        timestamp: 1681484753619,
+                        numberOfTransactions: 100
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+export default blockData;
