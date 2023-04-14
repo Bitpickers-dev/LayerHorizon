dfcfe665d5bbc5c404e201789d48d90a667ca4ee
ts 21 <shoukitsuda@icloud.com>
2023-04-14 17:37:33 +0900

    create selector ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-import styles from './page.module.css'
+"use client";
+
+import ChainOption from "@/types/ChainType";
+import { MultiValue } from "react-select";
+import ChainSelector from "./components/ChainSelector";
+import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedChain, setSelectedChain] = useState({} as MultiValue<ChainOption>);
+
+  const handleSelectChain = (chain: MultiValue<ChainOption>): void => {
+    setSelectedChain(chain);
+  };
+
   return (
     <main className={styles.main}>
+      <ChainSelector onSelectChain={handleSelectChain} />
     </main>
-  )
+  );
 }
