70e913114d9c98e52a17d8426d236b685f208c0a
mio256 <reiya@woorth.io>
2023-04-15 23:02:18 +0900

    :bug: 0x prefix in api method

--- src/api/getArbBatch.ts
+++ src/api/getArbBatch.ts
@@ -1,4 +1,4 @@
 import Block from "@/types/Block";
 
 export const getArbBatch = (eth: string): Promise<Block[]> =>
-  fetch(`http://127.0.0.1:8000/arbblock/?eth=${eth}`).then((x) => x.json());
+  fetch(`http://127.0.0.1:8000/arbblock/?eth=0x${eth}`).then((x) => x.json());
