2c13f847de6a2511c51d34c3098b966675b7fb95
ts 21 <shoukitsuda@icloud.com>
2023-04-15 16:12:50 +0900

    jump to etherscan

--- src/app/components/blocks/BlockDetailRow.tsx
+++ src/app/components/blocks/BlockDetailRow.tsx
@@ -10,38 +10,43 @@ type BlockDetailRowProps = {
   timestamp: number;
 };
 
+const Row = styled.a<BlockDetailRowProps>`
+  display: flex;
+  justify-content: space-between;
+  background-color: #fefefe;
+  width: 100%;
+  padding: 8px 16px;
+  border: none;
+`;
+
+const BlockNumber = styled.a`
+  font-size: 15px;
+  color: #818181;
+  text-align: right;
+`;
+const Age = styled.p`
+  font-size: 12px;
+  color: #818181;
+  text-align: right;
+`;
+
+const NumOfTx = styled.a`
+  font-size: 15px;
+  color: #818181;
+  text-align: right;
+`;
+
 const BlockDetailRow = (props: BlockDetailRowProps) => {
-  const BlockDetailRow = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 8px 16px;
-  `;
-
-  const BlockNumber = styled.a`
-    font-size: 15px;
-    color: #818181;
-    text-align: right;
-  `;
-
-  const Age = styled.p`
-    font-size: 12px;
-    color: #818181;
-    text-align: right;
-  `;
-
-  const NumOfTx = styled.a`
-    font-size: 15px;
-    color: #818181;
-    text-align: right;
-  `;
+  const handleClick = (): void => {
+    window.location.href = `https://etherscan.io/block/${props.blockNumber}`;
+  };
 
   return (
-    <BlockDetailRow>
+    <Row blockNumber={0} numberOfTransaction={0} onClick={handleClick} timestamp={0}>
       <BlockNumber>#{props.blockNumber}</BlockNumber>
       <Age>{dayjs.unix(props.timestamp).fromNow()}</Age>
       <NumOfTx>{props.numberOfTransaction} txns</NumOfTx>
-    </BlockDetailRow>
+    </Row>
   );
 };
 
