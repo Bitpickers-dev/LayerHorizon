a03b6b99179cd462cc6f606c933256aa3f1c091d
mio256 <reiya@woorth.io>
2023-04-16 02:32:32 +0900

    :sparkles: azure url

--- src/api/getArbBatch.ts
+++ src/api/getArbBatch.ts
@@ -1,4 +1,4 @@
 import Block from "@/types/Block";
 
 export const getArbBatch = (eth: string): Promise<Block[]> =>
-  fetch(`http://127.0.0.1:8000/arbblock/?eth=0x${eth}`).then((x) => x.json());
+  fetch(`https://layerhorizon-server.azurewebsites.net/arbblock/?eth=0x${eth}`,{mode:'cors'}).then((x) => x.json());
