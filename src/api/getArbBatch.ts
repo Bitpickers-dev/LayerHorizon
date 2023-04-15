4e2800221479e8043fdc9c8e82fb891fc389fced
SakamotoHiroya <ayorih16@icloud.com>
2023-04-16 03:57:53 +0900

    :zap: api_cors and fast_api

--- src/api/getArbBatch.ts
+++ src/api/getArbBatch.ts
@@ -1,5 +1,6 @@
 import Block from "@/types/Block";
 
 export const getArbBatch = (eth: string): Promise<Block[]> =>
-  // fetch(`http://127.0.0.1:8000/arbblock/?eth=${eth}`).then((x) => x.json());
-  fetch(`http://127.0.0.1:8000/arbblock/?eth=${eth}`).then((x) => x.json());
+  // fetch(`http://127.0.0.1:8000/arbblock/?eth=0x${eth}`).then((x) => x.json());
+  // fetch(`https://layerhorizon-server.azurewebsites.net/arbblock/?eth=0x${eth}`).then((x) => x.json());
+  fetch(`https://layerhorizon-server.azurewebsites.net/arbblock/?eth=0x${eth}&fields[]=count&fields[]=number&fields[]=timestamp`).then((x) => x.json());
