06718eabc3c8dd71de7ef46bf82cb239ce73e6a5
mio256 <reiya@woorth.io>
2023-04-16 05:50:49 +0900

    :bug: dubble 0x prefixs

--- src/api/getEthBlock.ts
+++ src/api/getEthBlock.ts
@@ -1,5 +1,5 @@
 import Block from "@/types/Block";
 
 export const getEthBlock = (number: string): Promise<Block> =>
-  // fetch(`http://127.0.0.1:8000/ethblock/0x${number}/`).then((x) => x.json());
-  fetch(`https://layerhorizon-server.azurewebsites.net/ethblock/0x${number}/`,{mode:'cors'}).then((x) => x.json());
+  // fetch(`http://127.0.0.1:8000/ethblock/${number}/`).then((x) => x.json());
+  fetch(`https://layerhorizon-server.azurewebsites.net/ethblock/${number}/`,{mode:'cors'}).then((x) => x.json());
