afcd038f171052db3439501f93bc4cd9f7b8d011
mio256 <reiya@woorth.io>
2023-04-15 17:14:24 +0900

    :bug: Response -> Block[]

--- src/api/getEthList.ts
+++ src/api/getEthList.ts
@@ -1,4 +1,4 @@
-import Response from "@/types/Response";
+import Block from "@/types/Block";
 
-export const getEthList = (length: number): Promise<Response> =>
+export const getEthList = (length: number): Promise<Block[]> =>
   fetch(`http://127.0.0.1:8000/ethblock/?size=${length}`).then((x) => x.json());
--- src/api/getOptBatch.ts
+++ src/api/getOptBatch.ts
@@ -1,4 +1,4 @@
-import Response from "@/types/Response";
+import Block from "@/types/Block";
 
-export const getOptBatch = (eth: string): Promise<Response> =>
+export const getOptBatch = (eth: string): Promise<Block[]> =>
   fetch(`http://127.0.0.1:8000/arbblock/?eth=${eth}`).then((x) => x.json());
